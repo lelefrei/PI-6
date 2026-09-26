3072d6293af5b2f7edac65f1b1d1cd8dd33712ab
Leticia Freitas <104323257+lelefrei@users.noreply.github.com>
2026-09-26 16:47:40 -0300

    app.js

--- app.js
+++ app.js
@@ -638,64 +638,6 @@ document.addEventListener(
         );
     }
 );
-// ============================================================
-// MODO NOTURNO
-// ============================================================
 
-const themeBtn =
-    document.getElementById("themeBtn");
-
-
-// Carrega o tema salvo
-const temaSalvo =
-    localStorage.getItem("tema");
-
-if (temaSalvo === "dark") {
-
-    document.body.classList.add("dark");
-
-    if (themeBtn) {
-        themeBtn.textContent = "☀️";
-    }
-
-}
-
-
-// Clique no botão
-if (themeBtn) {
-
-    themeBtn.addEventListener(
-        "click",
-        function() {
-
-            document.body.classList.toggle("dark");
-
-
-            const modoEscuro =
-                document.body.classList.contains("dark");
-
-
-            if (modoEscuro) {
-
-                themeBtn.textContent = "☀️";
-
-                localStorage.setItem(
-                    "tema",
-                    "dark"
-                );
-
-            } else {
-
-                themeBtn.textContent = "🌙";
-
-                localStorage.setItem(
-                    "tema",
-                    "light"
-                );
-
-            }
-
-        }
-    );
 
 }
